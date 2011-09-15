4362998f482ba461467a0fd4053ad6a4b20cb6c3
Eduard Fritscher <fritschereduard@Fritscher-Eduards-MacBook-Pro.local>
2011-09-15 11:45:29 +0200

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
+    @test = "test"
   end
 
   def contact
+    @title = "Contact"
   end
 
+  def about
+    @title = "About"
+  end
 end
